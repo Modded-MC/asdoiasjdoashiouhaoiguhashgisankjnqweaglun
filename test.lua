aaebddf52d6694af533567aa12c6dfd819f72b27
Modded-MC <126523110+Modded-MC@users.noreply.github.com>
2023-03-03 13:45:31 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,11 @@
+local function getPasswordInput()
+    term.clear()
+    term.setCursorPos(1,1)
+    print("ᴘᴀssᴡᴏʀᴅ > ")
+    local input = read("*")
+    if input == "fortnitebattlepass" then
+        print("hooray, u got")
+    end
+end
 
+getPasswordInput()
