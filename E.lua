43b88bc849bda5c09c00323f4dd5da32dbcc188b
Modded-MC <126523110+Modded-MC@users.noreply.github.com>
2023-03-03 13:59:49 +0000

    Update and rename Ea.lua to E.lua

--- E.lua
+++ E.lua
@@ -4,12 +4,14 @@ local function getKeyInput()
     term.write("Key > ");
     local input = read("*");
     if input == "MySuperCoolPassword123" then
-        getKeyInput();
+        term.write("Correct key entered, opening door.")
         redstone.setOutput("left", true);
         sleep(2)
         redstone.setOutput("left", false);
+        getKeyInput();
     else
         term.write("Incorrect key, try again.");
+        sleep(2);
         getKeyInput();
     end;
 end;
