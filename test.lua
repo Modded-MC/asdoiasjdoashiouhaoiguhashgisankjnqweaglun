85b588cc28ffa62edd8d6506d606e947139a4cde
Modded-MC <126523110+Modded-MC@users.noreply.github.com>
2023-03-03 13:47:36 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,15 @@
-local function getPasswordInput()
-    term.clear()
-    term.setCursorPos(1,1)
-    print("ᴘᴀssᴡᴏʀᴅ > ")
-    local input = read("*")
-    if input == "fortnitebattlepass" then
-        print("hooray, u got")
-    end
-end
+local function getKeyInput()
+    term.clear();
+    term.setCursorPos(0,0);
+    term.write("Key > ");
+    local input = read("*");
+    if input == "SpaceDoor..Open*!£" then
+        redstone.setOutput("left", true);
+        sleep(2)
+        redstone.setOutput("left", false);
+    else
+        print("Incorrect key, try again.");
+    end;
+end;
 
-getPasswordInput()
+getPasswordInput();
