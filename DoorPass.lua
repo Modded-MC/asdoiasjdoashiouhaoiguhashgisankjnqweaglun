c8cc988dc93e9968db7471f62a561ab3dd64d715
Modded-MC <126523110+Modded-MC@users.noreply.github.com>
2023-03-03 14:23:45 +0000

    Update and rename E.lua to DoorPass.lua

--- DoorPass.lua
+++ DoorPass.lua
@@ -3,16 +3,21 @@ local function getKeyInput()
     term.setCursorPos(1,1);
     term.write("Key > ");
     local input = read("*");
-    if input == "MySuperCoolPassword123" then
+    if input == "Door-Open1!!mbnlkap-13145" then
         term.write("Correct key entered, opening door.")
         redstone.setOutput("left", true);
         sleep(2)
         redstone.setOutput("left", false);
         getKeyInput();
     else
-        term.write("Incorrect key, try again.");
-        sleep(2);
-        getKeyInput();
+        if input ~= "CoreEnterPswd14!FGb-L" then
+            term.write("Incorrect key, try again.");
+            sleep(2);
+            getKeyInput();
+        else
+            term.write("Entered source key, exiting program.");
+            exit();
+        end;
     end;
 end;
 
