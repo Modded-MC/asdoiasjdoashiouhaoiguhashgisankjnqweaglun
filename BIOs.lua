db9395f81a3b7aeab6b262878599f41446562c69
Modded-MC <126523110+Modded-MC@users.noreply.github.com>
2023-03-04 19:50:02 +0000

    Update and rename BIOS.lua to BIOs.lua

--- BIOs.lua
+++ BIOs.lua
@@ -5,19 +5,23 @@ local function getBioS()
     term.setCursorPos(1,1);
     term.write("BioS > ");
     local input = read("*");
-    if input == "Device.PowerOn!" then
+    if input.tolower() == "device.poweron!" then
         term.write("Powering device on...")
         sleep(2)
+        term.setCursorPos(1,1);
         term.clear();
+        term.read("-")
     else
-        if input ~= "DevloperMode.Enable!" then
+        if input.tolower() ~= "devlopermode.enable!" then
             term.write("Device powering off...");
             sleep(2);
             term.clear();
             os.shutdown();
         else
-            term.write("Entered source key, exiting program.");
-            exit();
+            term.write("Enabled developer mode.. Exiting to system");
+            sleep(2);
+            term.clear();
+            term.setCursorPos(1,1);
         end;
     end;
 end;
