4e31ebe62d4eed1d948005befde595f138b8f315
Modded-MC <126523110+Modded-MC@users.noreply.github.com>
2023-03-05 00:53:43 +0000

    Update nsus.lua

--- nsus.lua
+++ nsus.lua
@@ -1,1 +1,32 @@
-e
+oldOsPullEvent = os.pullEvent
+os.pullEvent = os.pullEventRaw
+local function getBioS()
+    term.clear();
+    term.setCursorPos(1,1);
+    term.write("BioS > ");
+    local input = read("*");
+    if input == "password" then
+        term.write("Powering device on...")
+        shell.run("monitor top clear");
+        sleep(2)
+        term.setCursorPos(1,1);
+        term.clear();
+        shell.run("monitor top levelos")
+    else
+        if input ~= "password.dev" then
+            term.write("Device powering off...");
+            shell.run("monitor top clear");
+            sleep(2);
+            term.clear();
+            os.shutdown();
+        else
+            term.write("Enabled developer mode.. Exiting to system");
+            shell.run("monitor top clear");
+            sleep(2);
+            term.clear();
+            term.setCursorPos(1,1);
+        end;
+    end;
+end;
+
+getBioS();
